ca9016746e5e85b9e6612dcec900629ff74e1c06
Miracle pininee siddert Saturday <110922869+miraclesiddert@users.noreply.github.com>
2024-12-28 21:48:42 +0000

    privalex update

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// ==================== SHOW / HIDE NAV MENU
+
+const menu = document.querySelector(".nav__links")
+const menuBtn = document.querySelector("#open-menu-btn")
+const closeBtn = document.querySelector("#close-menu-btn")
+
+// to open the menu btn
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
